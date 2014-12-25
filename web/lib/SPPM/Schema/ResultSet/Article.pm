13cc7c0a365d495549c718f1c64fe0383cbfd77a
Renato CRON <renato.cron@gmail.com>
2014-12-25 17:05:52 -0200

    data das publicacoes (precisa rodar o json-to-db novamente)

--- web/lib/SPPM/Schema/ResultSet/Article.pm
+++ web/lib/SPPM/Schema/ResultSet/Article.pm
@@ -27,6 +27,30 @@ sub upsert {
 
     my $row = $self->search({ uri_path => $article->{uri_path} })->next;
 
+    if ($article->{article_type} eq 'article'){
+
+        ($article->{published_at}) = $article->{old_uri_path} =~ /(\d{4})/;
+
+        if (my ($d, $m, $y) = $article->{content} =~ /(\d{2})\s+([A-Za-z]{3})\s+(\d{2,4})/ ){
+
+
+            my %meses = qw/
+                Jan 01 Fev 02 Mar 03 Abr 04 Mai 05 Jun 06 Jul 07 Ago 08 Set 09 Out 10 Nov 11 Dez 12
+                Jan 01 Feb 02 Mar 03 Apr 04 May 05 Jun 06 Jul 07 Aug 08 Sep 09 Oct 10 Nov 11 Dec 12
+            /;
+
+            $m = ucfirst lc $m;
+            if (exists $meses{$m}){
+                $m = $meses{$m};
+                $article->{published_at} = "$y-$m-$d";
+            }
+
+        }
+
+        $article->{published_at} .= '-01-01' if $article->{published_at} && $article->{published_at} !~ /-/;
+
+    }
+
     if (!$row){
         $row = $self->create( $article );
     }else{
