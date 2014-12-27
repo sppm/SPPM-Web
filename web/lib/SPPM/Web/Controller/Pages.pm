2cc7d4b003888f46eb02acbfa32110c6865d6aea
Renato CRON <renato.cron@gmail.com>
2014-12-27 05:01:23 -0200

    pagina quem somos com alguns gravatares

--- web/lib/SPPM/Web/Controller/Pages.pm
+++ web/lib/SPPM/Web/Controller/Pages.pm
@@ -5,6 +5,25 @@ use utf8;
 
 BEGIN { extends 'Catalyst::Controller' }
 
+my %actions = (
+    'quem-somos' => sub {
+        my ($self, $c) = @_;
+
+        my @rows = $c->model('DB::Article')->search({
+            published => 1,
+        }, {
+            columns => ['me.author_hash', 'author_hash.name'],
+            group_by => ['me.author_hash', 'author_hash.name', 'author_hash.author_hash'],
+            order_by => \'random()',
+            rows => 12,
+            prefetch => 'author_hash'
+        })->all;
+
+        $c->stash->{authors} = {map { $_->get_column('author_hash') => $_->author_hash->name } @rows};
+
+
+    }
+);
 
 sub base: Chained('/root') PathPart('pagina') CaptureArgs(0) {
     my ( $self, $c ) = @_;
@@ -20,6 +39,8 @@ sub object: Chained('base') PathPart('') CaptureArgs(1) {
     if (-e $c->path_to('root', 'src', 'pages', "$page_name.tx")){
 
         $c->stash->{content_template} = "pages/$page_name.tx";
+
+        $actions{$page_name}($self, $c) if exists $actions{$page_name};
 
     }else{
         # 404
